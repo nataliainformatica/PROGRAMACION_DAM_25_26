8f953acd9865b9b0c33b415e20c1b99d0a4e2184
nataliainformatica <113176659+nataliainformatica@users.noreply.github.com>
2025-11-21 17:27:38 +0100

    Add files via upload

---
+++ UT5/ejemploTeoriaPrimerDia/Retos/Sudoku.java
@@ -0,0 +1,41 @@
+package es.avellaneda.clases;
+
+public class Sudoku {
+
+    int[][] sudoku;
+
+
+
+    private booleancomprobarSudoku(nt sudodku[][]){
+        // calculo los límites tengo 2 filas y dos columnas
+        for(int i=0; i< (sudoku.length/2) ;i++){
+            for(int j=0; j<(sudoku[i].length; j++) ; j++){
+                if(!comprobarCuadrado((sudoku.length/2),(sudoku[i].length/2), sudodku)){
+                    return false; 
+                }
+            }
+        }
+        return true; 
+         
+
+    }
+    private boolean comprobarCuadrado(int limiteInferior, int limiteSuperior, int sudodku[][]){
+
+       int comprobacion =0; 
+        for(int i = 0; i< limiteInferior;i++){
+            for (int j=0; j<limiteSuperior; j++){
+         
+                comprobacion += sudodku[i][j]; 
+
+            }
+        }
+        if(comprobacion !=10){
+            return true; 
+        }
+
+        return false; 
+
+
+    }
+
+}
