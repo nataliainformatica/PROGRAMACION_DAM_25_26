2309434be4a8d33be0968d8142e0fe7339e9dde4
nataliainformatica <113176659+nataliainformatica@users.noreply.github.com>
2026-03-05 16:24:05 +0100

    Add files via upload

---
+++ UT9/primerEjercicio/Principal.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        // dentro de la parte del programa de captura de datos
+
+        ArrayList<MiExcepcion> excepciones = new ArrayList<>(); 
+
+        // dame la matricula
+        String matricula;
+        do {
+            try {
+                // instancia de Coche
+
+            } catch (MiExcepcion e) {
+                excepciones.add(e); 
+
+                e.getHora(); 
+
+            }
+
+
+        } while (true);
+
+    }
+
+}
