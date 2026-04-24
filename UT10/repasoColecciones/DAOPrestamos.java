9454d118eecca0e7985b4406720449bd11e1b642
nataliainformatica <113176659+nataliainformatica@users.noreply.github.com>
2026-04-24 12:55:55 +0200

    Create DAOPrestamos.java

---
+++ UT10/repasoColecciones/DAOPrestamos.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class DAOPrestamos {
+
+    private Set<Prestamo> prestamos;
+
+    public DAOPrestamos() {
+        prestamos = new HashSet<>();
+    }
+
+    // TODO: implementar agregarPrestamo()
+    public boolean agregarPrestamo(Prestamo prestamo) {
+        return false;
+    }
+
+    // TODO: implementar buscarUltimoPrestamoPorSocio()
+    public Prestamo buscarUltimoPrestamoPorSocio(String idSocio) {
+        return null;
+    }
+}
